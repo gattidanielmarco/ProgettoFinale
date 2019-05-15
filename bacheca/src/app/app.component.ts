cddb61d3def004bb9f04be0ae0c6bfcb3175ec66
EC2 Default User <ec2-user@ip-172-31-94-208.ec2.internal>
2019-05-15 13:00:38 +0000

    Punto

--- bacheca/src/app/app.component.ts
+++ bacheca/src/app/app.component.ts
@@ -10,8 +10,10 @@ import { bacheca } from './bacheca';
 export class AppComponent {
   title = 'bacheca';
   myForm: FormGroup;
+  vettUtenti: bacheca[];
   input: bacheca;
   constructor(fb:FormBuilder){
+    this.vettUtenti=new Array();
     this.myForm = fb.group({
       'nome': ['',Validators.required],
       'cognome': ['',Validators.required],
@@ -23,7 +25,15 @@ export class AppComponent {
   }
   onSubmit(){
     if(!this.myForm.invalid){
-      let x = this.myForm.controls['nome'].value + this.myForm.controls['cognome'].value + this.myForm.controls['email'].value + this.myForm.controls['password'].value + this.myForm.controls['numeri'].value;
+      
+      this.input = new bacheca();
+      this.input.nome = this.myForm.controls['nome'].value;
+      this.input.cognome = this.myForm.controls['cognome'].value;
+      this.input.email = this.myForm.controls['email'].value;
+      this.input.password = this.myForm.controls['password'].value;
+      this.input.numeri = this.myForm.controls['numeri'].value;
+      
+      this.vettUtenti.push(this.input);
       
     }
   }
--- bacheca/src/app/bacheca.ts
+++ bacheca/src/app/bacheca.ts
@@ -1,11 +1,10 @@
 export class bacheca{
-    constructor(
-    nome: string,
-    cognome: string,
-    email: string,
-    password: string,
-    numero: string
-    ){}
+    nome: string;
+    cognome: string;
+    email: string;
+    password: string;
+    numeri: string;
+    
     
 
 }
