2f4ac5030cde6d0bd282d94ca4ca2f22474a99f1
EC2 Default User <ec2-user@ip-172-31-94-208.ec2.internal>
2019-05-15 12:00:31 +0000

    Progetto aggiornato

--- bacheca/src/app/app.component.ts
+++ bacheca/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { bacheca } from './bacheca';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bacheca';
+  myForm: FormGroup;
+  input: bacheca;
+  constructor(fb:FormBuilder){
+    this.myForm = fb.group({
+      'nome': ['',Validators.required],
+      'cognome': ['',Validators.required],
+      'email': ['',[Validators.required, Validators.email]],
+      'password': ['',Validators.required],
+      'numeri': ['',Validators.required]
+      
+    });
+  }
+  onSubmit(){
+    if(!this.myForm.invalid){
+      let x = this.myForm.controls['nome'].value + this.myForm.controls['cognome'].value + this.myForm.controls['email'].value + this.myForm.controls['password'].value + this.myForm.controls['numeri'].value;
+      
+    }
+  }
 }
---
+++ bacheca/src/app/bacheca.ts
@@ -0,0 +1,11 @@
+export class bacheca{
+    constructor(
+    nome: string,
+    cognome: string,
+    email: string,
+    password: string,
+    numero: string
+    ){}
+    
+
+}
