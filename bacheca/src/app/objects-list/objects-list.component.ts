9cff8f78a2944a1f4be71b5623d343647932a5f6
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2019-06-30 19:05:50 +0000

    creazione componenti

--- bacheca/src/app/objects-list/objects-list.component.ts
+++ bacheca/src/app/objects-list/objects-list.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { bacheca, loginDati } from '../bacheca';
+import { bacheca2 } from '../bacheca';
+import { commenti } from '../bacheca';
+import { risposte } from '../bacheca';
+import { Utenti } from '../mock-bacheca';
+import { Dati } from '../mock-bacheca';
+import { CommentoUtente } from '../mock-bacheca';
+import { Oggetto, oggetto1 } from '../new-object/new-object';
+import { Object } from '../new-object/mock-new-object';
 @Component({
   selector: 'app-objects-list',
   templateUrl: './objects-list.component.html',
   styleUrls: ['./objects-list.component.css']
 })
 export class ObjectsListComponent implements OnInit {
-
+@Input() ogg: Oggetto[] = Object;
   constructor() { }
 
   ngOnInit() {
